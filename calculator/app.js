5c6036962e938354196621c57f4f67b8a1ef8712
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2022-12-30 23:58:24 +0330

    arrange file

--- calculator/app.js
+++ calculator/app.js
@@ -1,16 +1,25 @@
 const span = document.querySelectorAll("span");
 const display = document.querySelector(".display");
-const clear=document.getElementById('clear')
+const clear = document.getElementById('clear')
 let number = 0;
 let operation = "";
 span.forEach((element) => {
   element.addEventListener("click", (e) => {
+    let value = e.originalTarget.innerHTML
     
-    display.innerHTML+=e.originalTarget.innerHTML
+    if(display.innerHTML.length <16){
+      display.innerHTML += value
+      return;
+    }
+
+    console.log(display.innerHTML.length)
+    let data = e.target.dataset.input;
+    console.log(data)
   });
 });
-clear.addEventListener("click", () =>{
-    display.innerHTML='';
+
+clear.addEventListener("click", () => {
+  display.textContent = '';
 })
 
 
